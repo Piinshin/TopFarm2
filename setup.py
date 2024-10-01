00ab6b42e5a3db2b4bec36cdb40e16c09e852a92
Piinshin <137865001+Piinshin@users.noreply.github.com>
2024-10-01 18:04:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(name='topfarm',
+setup(name='topfarm_v2',
       version=version,
       description='Topfarm - Wind farm optimization using OpenMDAO',
       long_description=read('README'),
